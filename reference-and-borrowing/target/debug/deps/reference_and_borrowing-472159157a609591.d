0941eede6c8ed1c09c07e50789de2dc526c7457e
jits98 <jits8809@gmail.com>
2026-02-03 09:10:23 +0530

    first example

---
+++ reference-and-borrowing/target/debug/deps/reference_and_borrowing-472159157a609591.d
@@ -0,0 +1,5 @@
+/Users/home/Desktop/projects/reference-and-borrowing/target/debug/deps/reference_and_borrowing-472159157a609591.d: src/main.rs
+
+/Users/home/Desktop/projects/reference-and-borrowing/target/debug/deps/reference_and_borrowing-472159157a609591: src/main.rs
+
+src/main.rs:
---
+++ reference-and-borrowing/target/debug/reference-and-borrowing.d
@@ -0,0 +1,1 @@
+/Users/home/Desktop/projects/reference-and-borrowing/target/debug/reference-and-borrowing: /Users/home/Desktop/projects/reference-and-borrowing/src/main.rs
