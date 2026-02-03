4a17b09797dcbcf02ea782f483e13e875da84727
jits98 <jits8809@gmail.com>
2026-02-03 10:00:16 +0530

    last example of borrowing and reference chapter of rust-official docs

--- reference-and-borrowing/src/main.rs
+++ reference-and-borrowing/src/main.rs
@@ -1,25 +1,25 @@
 fn main() {
+    let reference_to_nothing = dangle();
+    println!("{}", reference_to_nothing);
+
     // let s1 = String::from("hello");
-
     // let len = calculate_length(&s1);
-
     // println!("The length of '{s1}' is {len}.");
-
-    let mut s = String::from("hello");
-
+    // let mut s = String::from("hello");
     // {let r1 = &mut s;}
     // let r2 = &mut s;
-
-    let r1 = &s;
-    let r2 = &s;
-
-    println!("{r1} and {r2}");
-
-    let r3 = &mut s;
-    println!("{r3}");
-    
+    // let r1 = &s;
+    // let r2 = &s;
+    // println!("{r1} and {r2}");
+    // let r3 = &mut s;
+    // println!("{r3}");
     // change(&mut s);
+}
 
+fn dangle() -> String {
+    let s = String::from("hello");
+
+    s
 }
 
 // fn calculate_length(s: & String) -> usize {
