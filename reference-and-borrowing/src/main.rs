aeadbee1d538793b186f4c8dde3c95f8882450ac
jits98 <jits8809@gmail.com>
2026-02-03 09:36:22 +0530

    rust example

--- reference-and-borrowing/src/main.rs
+++ reference-and-borrowing/src/main.rs
@@ -1,11 +1,31 @@
 fn main() {
-    let s1 = String::from("hello");
+    // let s1 = String::from("hello");
 
-    let len = calculate_length(&s1);
+    // let len = calculate_length(&s1);
 
-    println!("The length of '{s1}' is {len}.");
+    // println!("The length of '{s1}' is {len}.");
+
+    let mut s = String::from("hello");
+
+    // {let r1 = &mut s;}
+    // let r2 = &mut s;
+
+    let r1 = &s;
+    let r2 = &s;
+
+    println!("{r1} and {r2}");
+
+    let r3 = &mut s;
+    println!("{r3}");
+    
+    // change(&mut s);
+
 }
 
-fn calculate_length(s: & String) -> usize {
-    s.len()
-}
+// fn calculate_length(s: & String) -> usize {
+//     s.len()
+// }
+
+// fn change(some_string: &mut String) {
+//     some_string.push_str(", world");
+// }
