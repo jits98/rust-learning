1472af5d2075722971ea6c7dbe3ce1898ba3c7dc
jits98 <jits8809@gmail.com>
2026-02-04 09:31:21 +0530

    read slice chapter

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -1,12 +1,32 @@
 fn main() {
 
-    let mut s = String::from("hello world");
+    let a = [1,2,3,4,5];
 
-    let word = first_word(&s);
+    let slice = &a[1..3];
 
-    s.clear();
+    assert_eq!(slice, &[2,3]);
 
-    println!("the first word is: {word}");
+    // let my_string = String::from("hello world");
+
+    // let word = first_word(&my_string[0..6]);
+    // let word = first_word(&my_string[..]);
+
+    // let word = first_word(&my_string);
+
+    // let my_string_literal = "hello world";
+
+    // let word = first_word(&my_string_literal[0..6]);
+    // let word = first_word(&my_string_literal[..]);
+
+    // let word = first_word(my_string_literal);
+
+    // let mut s = String::from("hello world");
+
+    // let word = first_word(&s);
+
+    // s.clear();
+
+    // println!("the first word is: {word}");
     // let s = String::from("hello world");
 
     // let hello = &s[0..5];
@@ -50,14 +70,14 @@ fn main() {
 //     s.len()
 // }
 
-fn first_word(s: &String) -> &str {
-    let bytes = s.as_bytes();
+// fn first_word(s: &str) -> &str {
+//     let bytes = s.as_bytes();
 
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..i];
-        }
-    }
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return &s[0..i];
+//         }
+//     }
 
-    &s[..]
-}
+//     &s[..]
+// }
