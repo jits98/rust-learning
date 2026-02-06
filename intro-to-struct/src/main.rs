cf5adc3fb3cca02150f0b767d0be35ddf6dbdb72
jits98 <jits8809@gmail.com>
2026-02-06 08:54:52 +0530

    tuple struct example

--- intro-to-struct/src/main.rs
+++ intro-to-struct/src/main.rs
@@ -1,15 +1,17 @@
 fn main() {
-    let user1 = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
-        sign_in_count: 1,
-    };
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+    // let user1 = User {
+    //     active: true,
+    //     username: String::from("someusername123"),
+    //     email: String::from("someone@example.com"),
+    //     sign_in_count: 1,
+    // };
 
-    let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1
-    };
+    // let user2 = User {
+    //     email: String::from("another@example.com"),
+    //     ..user1
+    // };
 
     // let user2 = User {
     //     active: user1.active,
@@ -20,16 +22,17 @@ fn main() {
 
     // user1.email = String::from("anotheremail@example.com");
 }
-
+struct Color (i32, i32, i32);
+struct Point (i32, i32, i32);
 //Using the Field Init Shorthand
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        username,
-        email,
-        sign_in_count: 1,
-    }
-}
+// fn build_user(email: String, username: String) -> User {
+//     User {
+//         active: true,
+//         username,
+//         email,
+//         sign_in_count: 1,
+//     }
+// }
 
 // fn build_user(email: String, username: String) -> User {
 //     User {
@@ -40,9 +43,9 @@ fn build_user(email: String, username: String) -> User {
 //     }
 // }
 
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
+// struct User {
+//     active: bool,
+//     username: String,
+//     email: String,
+//     sign_in_count: u64,
+// }
