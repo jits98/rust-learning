70e51fe875b3e3b3e7a215d01bf16ee454d48d45
jits98 <jits8809@gmail.com>
2026-02-06 08:25:33 +0530

    started struct chapter-5

---
+++ intro-to-struct/target/debug/deps/intro_to_struct-d45adeee6c607f2a.d
@@ -0,0 +1,5 @@
+/Users/home/Desktop/projects/intro-to-struct/target/debug/deps/intro_to_struct-d45adeee6c607f2a.d: src/main.rs
+
+/Users/home/Desktop/projects/intro-to-struct/target/debug/deps/libintro_to_struct-d45adeee6c607f2a.rmeta: src/main.rs
+
+src/main.rs:
---
+++ intro-to-struct/target/debug/deps/intro_to_struct-fd4114c43681c529.d
@@ -0,0 +1,5 @@
+/Users/home/Desktop/projects/intro-to-struct/target/debug/deps/intro_to_struct-fd4114c43681c529.d: src/main.rs
+
+/Users/home/Desktop/projects/intro-to-struct/target/debug/deps/libintro_to_struct-fd4114c43681c529.rmeta: src/main.rs
+
+src/main.rs:
