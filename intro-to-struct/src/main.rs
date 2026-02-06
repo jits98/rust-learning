37a4bd63824507619bbc8278abf7b0aaf268567b
jits98 <jits8809@gmail.com>
2026-02-06 08:32:05 +0530

    doing examples of struct chapter

--- intro-to-struct/src/main.rs
+++ intro-to-struct/src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
-    println!("Hello world!");
+    let user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
 }
 
 struct User {
