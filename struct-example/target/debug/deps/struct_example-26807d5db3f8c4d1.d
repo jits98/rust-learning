f30703f9920d1f8a15929fd8c093d56fe2a4ccdb
jits98 <jits8809@gmail.com>
2026-02-07 09:04:07 +0530

    struct example chapter start

---
+++ struct-example/target/debug/deps/struct_example-26807d5db3f8c4d1.d
@@ -0,0 +1,5 @@
+/Users/home/Desktop/projects/struct-example/target/debug/deps/struct_example-26807d5db3f8c4d1.d: src/main.rs
+
+/Users/home/Desktop/projects/struct-example/target/debug/deps/libstruct_example-26807d5db3f8c4d1.rmeta: src/main.rs
+
+src/main.rs:
---
+++ struct-example/target/debug/deps/struct_example-b9254e42230b1f25.d
@@ -0,0 +1,5 @@
+/Users/home/Desktop/projects/struct-example/target/debug/deps/struct_example-b9254e42230b1f25.d: src/main.rs
+
+/Users/home/Desktop/projects/struct-example/target/debug/deps/libstruct_example-b9254e42230b1f25.rmeta: src/main.rs
+
+src/main.rs:
