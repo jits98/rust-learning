38e070f8dcf48dce39640d817e34648bd6770df4
jits98 <jits8809@gmail.com>
2026-02-07 09:39:52 +0530

    struct example chapter 5.2 done

--- struct-example/src/main.rs
+++ struct-example/src/main.rs
@@ -1,11 +1,13 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 30,
+        width: dbg!(30 * scale),
         height: 50,
     };
 
@@ -14,7 +16,8 @@ fn main() {
     // let width1 = 30;
     // let height1 = 50;
 
-    println!("rect1 is {rect1:?}");
+    dbg!(&rect1);
+    println!("rect1 is {rect1:#?}");
     println!(
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
