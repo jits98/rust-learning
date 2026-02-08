d4bcd97f29f7bb9148ac898c68ef08038e515985
jits98 <jits8809@gmail.com>
2026-02-08 09:58:45 +0530

    methods with more parameters example

--- methods/src/main.rs
+++ methods/src/main.rs
@@ -5,9 +5,19 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    fn area (&self) -> u32 {
+    fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    // fn width (&self) -> bool {
+    //     self.width > 0
+    // }
+    // fn area (&self) -> u32 {
+    //     self.width * self.height
+    // }
 }
 fn main() {
     let rect1 = Rectangle {
@@ -15,5 +25,19 @@ fn main() {
         height: 50,
     };
 
-    println!("The area of the rectangle is {} square pixels.", rect1.area());
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("The area of rectangle 1 is: {}", rect1.area());
+    println!("The area of rectangle 2 is: {}", rect2.area());
+    println!("The area of rectangle 3 is: {}", rect3.area());
 }
