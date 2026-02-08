a66dbd620dd918bd4c32d539f465d79bb68ec822
jits98 <jits8809@gmail.com>
2026-02-08 10:14:20 +0530

    completed methods chapter 5.3

--- methods/src/main.rs
+++ methods/src/main.rs
@@ -5,25 +5,31 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    fn square(size: u32) -> Self {
-        Self {
-            width: size,
-            height: size,
-        }
-    }
+    // fn square(size: u32) -> Self {
+    //     Self {
+    //         width: size,
+    //         height: size,
+    //     }
+    // }
     fn area(&self) -> u32 {
         self.width * self.height
     }
 
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
+    // fn can_hold(&self, other: &Rectangle) -> bool {
+    //     self.width > other.width && self.height > other.height
+    // }
     // fn width (&self) -> bool {
     //     self.width > 0
     // }
     // fn area (&self) -> u32 {
     //     self.width * self.height
     // }
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 fn main() {
     let rect1 = Rectangle {
