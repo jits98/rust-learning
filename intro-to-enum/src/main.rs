5f7b6c943388b7f98b40fc5c1f1fd43abdc7708f
jits98 <jits8809@gmail.com>
2026-02-09 09:31:52 +0530

    enum chapter 6.1 complete

--- intro-to-enum/src/main.rs
+++ intro-to-enum/src/main.rs
@@ -1,22 +1,35 @@
 fn main() {
     println!("Hello, world!");
+    let x : i8 = 5;
+    let y: Option<i8> = Some(5);
+    
+    let sum = x + y;
 }
 
-enum Message {
-    Quit,
-    Move {x: i32, y: i32},
-    Write(String),
-    ChangeColor(i32, i32, i32),
-}
 
-impl Message {
-    fn call (&self) {
+// let some_number = Some(5);
+// let some_char = Some('e');
+// let absent_number: Option<i32> = None;
+// enum Option<T> {
+//     None,
+//     Some(T),
+// }
 
-    }
-}
+// enum Message {
+//     Quit,
+//     Move {x: i32, y: i32},
+//     Write(String),
+//     ChangeColor(i32, i32, i32),
+// }
 
-let m = Message::Write(String::from("hello"));
-m.call();
+// impl Message {
+//     fn call (&self) {
+
+//     }
+// }
+
+// let m = Message::Write(String::from("hello"));
+// m.call();
 
 // struct QuitMessage;
 // struct MoveMessage {
---
+++ intro-to-enum/target/debug/deps/intro_to_enum-a412033ad1e86eac.d
@@ -0,0 +1,5 @@
+/Users/home/Desktop/projects/intro-to-enum/target/debug/deps/intro_to_enum-a412033ad1e86eac.d: src/main.rs
+
+/Users/home/Desktop/projects/intro-to-enum/target/debug/deps/intro_to_enum-a412033ad1e86eac: src/main.rs
+
+src/main.rs:
