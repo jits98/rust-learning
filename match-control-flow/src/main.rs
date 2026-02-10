9c52f6a63b8ac4f91153aa136938c5122631a3b1
jits98 <jits8809@gmail.com>
2026-02-10 09:30:38 +0530

    completed chapter 6.2

--- match-control-flow/src/main.rs
+++ match-control-flow/src/main.rs
@@ -3,19 +3,21 @@ fn main () {
     // let five = Some(5);
     // let six = plus_one(five);
     // let none = plus_one(None);
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (),
+        // other => move_player(other),
+    }
 }
 
 
-let dice_roll = 9;
-match dice_roll {
-    3 => add_fancy_hat(),
-    7 => remove_fancy_hat(),
-    other => move_player(other),
-}
 
 fn add_fancy_hat() {}
 fn remove_fancy_hat() {}
-fn move_player(num_spaces: u8) {}
+// fn reroll() {}
+// fn move_player(num_spaces: u8) {}
 // fn plus_one(x: Option<i32>) -> Option<i32> {
 //     match x {
 //         // None => None,
