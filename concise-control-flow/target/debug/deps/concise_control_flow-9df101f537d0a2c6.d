4712f069dc51ecc967859ae2ad8ec9ec4d9d2531
jits98 <jits8809@gmail.com>
2026-02-11 08:42:11 +0530

    example 6.6 in chapter 6.3

---
+++ concise-control-flow/target/debug/deps/concise_control_flow-9df101f537d0a2c6.d
@@ -0,0 +1,5 @@
+/Users/home/Desktop/projects/concise-control-flow/target/debug/deps/concise_control_flow-9df101f537d0a2c6.d: src/main.rs
+
+/Users/home/Desktop/projects/concise-control-flow/target/debug/deps/libconcise_control_flow-9df101f537d0a2c6.rmeta: src/main.rs
+
+src/main.rs:
---
+++ concise-control-flow/target/debug/deps/concise_control_flow-edeb2a4d97d95ed0.d
@@ -0,0 +1,5 @@
+/Users/home/Desktop/projects/concise-control-flow/target/debug/deps/concise_control_flow-edeb2a4d97d95ed0.d: src/main.rs
+
+/Users/home/Desktop/projects/concise-control-flow/target/debug/deps/libconcise_control_flow-edeb2a4d97d95ed0.rmeta: src/main.rs
+
+src/main.rs:
