2bbf71082825497e73e7e89da2057a2b697ec1e7
jits98 <jits8809@gmail.com>
2026-02-11 09:05:21 +0530

    did example 6.8

--- concise-control-flow/src/main.rs
+++ concise-control-flow/src/main.rs
@@ -1,24 +1,37 @@
 fn main() {
-    impl UsState {
-        fn existed_in(&self, year: u16) -> bool {
-            match self {
-                UsState::Alabama => year >= 1819,
-                UsState::Alaska => year >= 1959,
-            }
-        }
-    }
-
     fn describe_state_quarter(coin: Coin) -> Option<String> {
-        if let Coin::Quarter(state) = coin {
-            if state.existed_in(1900) {
-                Some(format!("{state:?} is pretty old, for America!"))
-            } else {
-                Some(format!("{state:?} is relatively new."))
-            }
+        let state = if let Coin::Quarter(state) = coin {
+            state
         } else {
-            None
+            return None;
+        };
+
+        if state.existed_in(1900) {
+            Some(format!("{state:?} is pretty old, for America!"))
+        } else {
+            Some(format!("{state:?} is relatively new."))
         }
     }
+    // impl UsState {
+    //     fn existed_in(&self, year: u16) -> bool {
+    //         match self {
+    //             UsState::Alabama => year >= 1819,
+    //             UsState::Alaska => year >= 1959,
+    //         }
+    //     }
+    // }
+
+    // fn describe_state_quarter(coin: Coin) -> Option<String> {
+    //     if let Coin::Quarter(state) = coin {
+    //         if state.existed_in(1900) {
+    //             Some(format!("{state:?} is pretty old, for America!"))
+    //         } else {
+    //             Some(format!("{state:?} is relatively new."))
+    //         }
+    //     } else {
+    //         None
+    //     }
+    // }
     // let mut count = 0;
     // if let Coin::Quarter(state) = coin {
     //     println!("State quarter from {state:?}!");
