0b9e7ab77fa0f0f27345b7c6cce919748462ec2e
jits98 <jits8809@gmail.com>
2026-03-17 09:49:40 +0530

    did example 7-15

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,18 +1,28 @@
+use std::fmt;
+use std::io;
+
+fn function1() -> fmt::Result {}
+
+fn function2() -> io::Result<()> {}
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
     }
 }
 
-use crate::front_of_house::hosting;
+use crate::front_of_house::hosting::add_to_waitlist;
 
-mod customer {
 
-    pub fn eat_at_restaurant() {
-        hosting::add_to_waitlist();
-    }
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
 }
 
+// use std::collections::HashMap;
+
+// fn main() {
+//     let mut map = HashMap::new();
+//     map.insert(1,2);
+// }
 // mod back_of_house {
 //     pub enum Appetizer {
 //         Soup,
