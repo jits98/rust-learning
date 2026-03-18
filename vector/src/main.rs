9e025e8efae0a6c9079270b8b13f5d5898784939
jits98 <jits8809@gmail.com>
2026-03-18 12:54:07 +0530

    example 8-9

--- vector/src/main.rs
+++ vector/src/main.rs
@@ -42,10 +42,23 @@ fn main() {
     //     println!("{i}");
     // }
 
-    let mut v = vec![100,32,57];
-    for i in &mut v {
-        *i += 50;
-        println!("{i}");
+    // let mut v = vec![100,32,57];
+    // for i in &mut v {
+    //     *i += 50;
+    //     println!("{i}");
+    // }
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
     }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheeCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12);
+
+    ]
 
 }
