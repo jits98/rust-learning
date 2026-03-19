eddbcc6ee2de0b27cb0fc06c7ac23df408635443
jits98 <jits8809@gmail.com>
2026-03-19 12:19:35 +0530

    example 8-18

--- string_chapter/src/main.rs
+++ string_chapter/src/main.rs
@@ -1,35 +1,57 @@
 fn main() {
-    let hello = String::from("السلام عليكم");
-    println!("{}", hello);
-    let hello = String::from("Dobrý den");
-    println!("{}", hello);
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
 
-    let hello = String::from("Hello");
-    println!("{}", hello);
+    let s = format!("{s1}-{s2}-{s3}");
+    // let s = s1 + "-" + &s2 + "-" + &s3;
+    println!("{}", s); 
 
-    let hello = String::from("שלום");
-    println!("{}", hello);
+    // let s1 = String::from("Hello, ");
+    // let s2 = String::from("world! ");
+    // let s3 = s1 + &s2;
+    // println!("{s3}");
+    // println!("The length of s3 is {}", s3.len());
 
-    let hello = String::from("नमस्ते");
-    println!("{}", hello);
+    // let mut s = String::from("lo");
+    // s.push('l');
+    // println!("{s}");
+    // let mut s1 = String::from("foo");
+    // let s2 = "bar";
+    // s1.push_str(s2);
+    // println!("s1 is {s1}");
+    // println!("s2 is {s2}");
+    // let hello = String::from("السلام عليكم");
+    // println!("{}", hello);
+    // let hello = String::from("Dobrý den");
+    // println!("{}", hello);
 
-    let hello = String::from("こんにちは");
-    println!("{}", hello);
+    // let hello = String::from("Hello");
+    // println!("{}", hello);
 
-    let hello = String::from("안녕하세요");
-    println!("{}", hello);
+    // let hello = String::from("שלום");
+    // println!("{}", hello);
 
-    let hello = String::from("你好");
-    println!("{}", hello);
+    // let hello = String::from("नमस्ते");
+    // println!("{}", hello);
 
-    let hello = String::from("Olá");
-    println!("{}", hello);
+    // let hello = String::from("こんにちは");
+    // println!("{}", hello);
 
-    let hello = String::from("Здравствуйте");
-    println!("{}", hello);
+    // let hello = String::from("안녕하세요");
+    // println!("{}", hello);
 
-    let hello = String::from("Hola");
-    println!("{}", hello);
+    // let hello = String::from("你好");
+    // println!("{}", hello);
+
+    // let hello = String::from("Olá");
+    // println!("{}", hello);
+
+    // let hello = String::from("Здравствуйте");
+    // println!("{}", hello);
+
+    // let hello = String::from("Hola");
+    // println!("{}", hello);
 
 
     // let s = String::from("initial contents");
