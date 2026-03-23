eba2d5420d1afdfbca2fa181c764682be28c3dcd
jits98 <jits8809@gmail.com>
2026-03-23 10:55:42 +0530

    example 9-2

--- error_chapter/src/main.rs
+++ error_chapter/src/main.rs
@@ -1,5 +1,12 @@
-fn main() {
-    let v = vec![1,2,3];
+use std::fs::File;
 
-    v[99];
+fn main() {
+    let greeting_file_result = File::open("hello.txt");
 }
+
+
+// fn main() {
+//     let v = vec![1,2,3];
+
+//     v[99];
+// }
