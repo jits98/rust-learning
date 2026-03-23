4b3efb42e8344745ad2c6b6807c690b7dd7146e3
jits98 <jits8809@gmail.com>
2026-03-23 16:13:12 +0530

    example 9-9 -> Using fs::read_to_string instead of opening and then reading the file

--- error_chapter/src/main.rs
+++ error_chapter/src/main.rs
@@ -1,16 +1,36 @@
-use std::fs::File;
-use std::io::{self, Read};
+use std::fs;
+use std::io;
 
 fn main() {
-
+    
 }
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let mut username_file = File::open("hello.txt")?;
-    let mut username = String::new();
-    username_file.read_to_string(&mut username)?;
-    Ok(username);
+    fs::read_to_string("hello.txt")
 }
+
+
+// use std::fs::File;
+// use std::io::{self, Read};
+
+// fn main() {
+
+// }
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let mut username = String::new();
+
+//     File::open("hello.txt")?.read_to_string(&mut username)?;
+
+//     Ok(username)
+// }
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let mut username_file = File::open("hello.txt")?;
+//     let mut username = String::new();
+//     username_file.read_to_string(&mut username)?;
+//     Ok(username);
+// }
 
 // fn read_username_from_file() -> Result<String, io::Error> {
 //     let username_file_result = File::open("hello.txt");
