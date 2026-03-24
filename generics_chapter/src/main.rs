4c2f3e563de818e239ce019c0c30bd9059b8b715
jits98 <jits8809@gmail.com>
2026-03-24 16:30:36 +0530

    example 10-3 -> Abstracted code to find the largest number in two lists

--- generics_chapter/src/main.rs
+++ generics_chapter/src/main.rs
@@ -1,25 +1,46 @@
 fn main() {
+
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let mut largest = &number_list[0];
+    let result = largest(&number_list);
+    println!("The largest number is {result}");
 
-    for number in &number_list {
-        if number > largest {
-            largest = number;
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = largest(&number_list);
+    println!("The largest number is {result}");
+    // let number_list = vec![34, 50, 25, 100, 65];
+
+    // let mut largest = &number_list[0];
+
+    // for number in &number_list {
+    //     if number > largest {
+    //         largest = number;
+    //     }
+    // }
+
+    // println!("The largest number is {largest}");
+
+    // let number_list2 = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    // let mut largest2 = &number_list2[0];
+
+    // for number2 in &number_list2 {
+    //     if number2 > largest2 {
+    //         largest2 = number2;
+    //     }
+    // }
+
+    // println!("The largest number is {largest2}");
+}
+
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
         }
     }
-
-    println!("The largest number is {largest}");
-
-    let number_list2 = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-
-    let mut largest2 = &number_list2[0];
-
-    for number2 in &number_list2 {
-        if number2 > largest2 {
-            largest2 = number2;
-        }
-    }
-
-    println!("The largest number is {largest2}");
+    largest
 }
