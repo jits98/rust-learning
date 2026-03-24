83a272285967dd5d71c3e1cb9259eb530ab25eec
jits98 <jits8809@gmail.com>
2026-03-24 10:57:01 +0530

    example 9-13 -> A guess type that will only continue with values between 1 and 100

--- error_chapter/src/main.rs
+++ error_chapter/src/main.rs
@@ -1,11 +1,54 @@
-use std::fs::File;
+use std::net::IpAddr;
 
-fn main() -> Resutl<(), Box<dyn Error>> {
-    let greeting_file = File::open("hello.txt")?;
+fn main() {
 
-    Ok(());
-    // let greeting_file = File::open("hello.txt")?;
+    pub struct Guess {
+        value: i32,
+    }
+
+    impl Guess {
+        pub fn new(value: i32) -> Guess {
+            if value < 1 || value > 100 {
+                panic!("Guess value must be between 1 and 100, got {value}.")
+            }
+            Guess { value }
+        }
+
+        pub fn value(&self) -> i32 {
+            self.value
+        }
+    }
+    
+    // let home: IpAddr = "127.0.0.1"
+    //     .parse()
+    //     .expect("Hardcoded IP address should be valid");
+
+    // loop {
+    //     let guess: i32 = match guess.trim().parse() {
+    //         Ok(num) => num,
+    //         Err(_) => continue,
+    //     };
+
+    //     if guess < 1 || guess > 100 {
+    //         println!("The secret number will be between 1 and 100.");
+    //     }
+
+    //     match guess.cmp(&secret_number){
+
+    //     }
+    // }
+
 }
+
+
+// use std::fs::File;
+
+// fn main() -> Resutl<(), Box<dyn Error>> {
+//     let greeting_file = File::open("hello.txt")?;
+
+//     Ok(());
+//     // let greeting_file = File::open("hello.txt")?;
+// }
 
 // fn last_char_of_first_line(text: &str) -> Option<char> {
 //     text.lines().next()?.chars().last();
