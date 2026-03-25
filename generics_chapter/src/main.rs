cac5c2aa0bf690df5c9369c9e355bda2f4092ce0
jits98 <jits8809@gmail.com>
2026-03-25 10:25:50 +0530

    start chapter 10-1

--- generics_chapter/src/main.rs
+++ generics_chapter/src/main.rs
@@ -1,46 +1,50 @@
-fn main() {
+ 
 
-    let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+// fn main() {
 
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
-    // let number_list = vec![34, 50, 25, 100, 65];
+//     let number_list = vec![34, 50, 25, 100, 65];
 
-    // let mut largest = &number_list[0];
+//     let result = largest(&number_list);
+//     println!("The largest number is {result}");
 
-    // for number in &number_list {
-    //     if number > largest {
-    //         largest = number;
-    //     }
-    // }
+//     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
-    // println!("The largest number is {largest}");
+//     let result = largest(&number_list);
+//     println!("The largest number is {result}");
+//     // let number_list = vec![34, 50, 25, 100, 65];
 
-    // let number_list2 = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+//     // let mut largest = &number_list[0];
 
-    // let mut largest2 = &number_list2[0];
+//     // for number in &number_list {
+//     //     if number > largest {
+//     //         largest = number;
+//     //     }
+//     // }
 
-    // for number2 in &number_list2 {
-    //     if number2 > largest2 {
-    //         largest2 = number2;
-    //     }
-    // }
+//     // println!("The largest number is {largest}");
 
-    // println!("The largest number is {largest2}");
-}
+//     // let number_list2 = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
-fn largest(list: &[i32]) -> &i32 {
-    let mut largest = &list[0];
+//     // let mut largest2 = &number_list2[0];
 
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-    largest
-}
+//     // for number2 in &number_list2 {
+//     //     if number2 > largest2 {
+//     //         largest2 = number2;
+//     //     }
+//     // }
+
+//     // println!("The largest number is {largest2}");
+// }
+
+// fn largest(list: &[i32]) -> &i32 {
+//     let mut largest = &list[0];
+
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
