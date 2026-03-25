54b4641e1f67b90e30dce60b6c94e5f8d76bb65e
jits98 <jits8809@gmail.com>
2026-03-25 10:52:06 +0530

    example 10-7: The fields x and y must be the same type because both have the same generic data type T.

--- generics_chapter/src/main.rs
+++ generics_chapter/src/main.rs
@@ -1,3 +1,5 @@
+
+#[derive(Debug)]
 struct Point<T> {
     x: T,
     y: T,
@@ -6,6 +8,9 @@ struct Point<T> {
 fn main() {
     let integer = Point {x:5, y: 10};
     let float = Point {x: 1.0, y: 4.0};
+    println!("{:?}", integer);
+    println!("{:?}", float);
+    let wont_work = Point { x: 5, y: 4.0};
 }
 
 
