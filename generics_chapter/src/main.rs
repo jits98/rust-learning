f5e08eeb6a391a1363cab43ffdc6242d6cb49e6e
jits98 <jits8809@gmail.com>
2026-03-25 10:46:55 +0530

    example 10-6: A Point<T> struct that holds x and y values of type T

--- generics_chapter/src/main.rs
+++ generics_chapter/src/main.rs
@@ -1,3 +1,14 @@
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+fn main() {
+    let integer = Point {x:5, y: 10};
+    let float = Point {x: 1.0, y: 4.0};
+}
+
+
 //  fn largest_i32(list: &[i32]) -> &i32 {
 //     let mut largest = &list[0];
 
@@ -20,28 +31,28 @@
 //     largest
 //  }
 
-fn largest<T>(list: &[T]) -> &T {
-    let mut largest = &list[0];
+// fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+//     let mut largest = &list[0];
 
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-    largest
-}
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
 
- fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+//  fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
+//     let result = largest(&number_list);
+//     println!("The largest number is {result}");
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
+//     let char_list = vec!['y', 'm', 'a', 'q'];
     
-    let result = largest(&char_list);
-    println!("The largest char is {result}");
- }
+//     let result = largest(&char_list);
+//     println!("The largest char is {result}");
+//  }
 
 
 
