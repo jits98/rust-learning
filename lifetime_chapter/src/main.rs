a818fd35caa5ee5832fde3e9c7b3e2d304609a41
jits98 <jits8809@gmail.com>
2026-03-27 11:48:14 +0530

    example 10-25: A function we defined in Listing 4-9 that compiled without lifetime annotations, even though the parameter and return type are references

--- lifetime_chapter/src/main.rs
+++ lifetime_chapter/src/main.rs
@@ -1,25 +1,62 @@
-fn main() {
-    let string1 = String::from("long string is long");
-    let string2 = String::from("xyz");
+// #[derive(Debug)]
+// struct ImportantExcerpt<'a> {
+//     part: &'a str,
+// }
 
-    let result = longest(string1.as_str(), string2.as_str());
-    println!("The longest string is {result}");
+// fn main() {
+//     let novel = String::from("Call me Ishmael. Some years ago...");
+//     let first_sentence = novel.split('.').next().unwrap();
+//     let i = ImportantExcerpt {
+//         part: first_sentence,
+//     };
 
-    // let x = 5;
+//     println!("{:?}", i);
+// }
 
-    // let r = &x;
-    // &i32;
-    // &'a i32;
-    // &'a mut i32;
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
 
-    // // {
-    // //     let x = 5;
-    // //     r = &x;
-    // // }
-
-    // println!("r: {r}");
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
 }
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() { x } else { y }
-}
+fn main() {}
+
+
+
+// fn main() {
+//     let string1 = String::from("long string is long");
+//     let result;
+//     {
+//         let string2 = String::from("xyz");
+
+//         result = longest(string1.as_str(), string2.as_str());
+//     }
+//     println!("The longest string is {result}");
+
+
+//     // let x = 5;
+
+//     // let r = &x;
+//     // &i32;
+//     // &'a i32;
+//     // &'a mut i32;
+
+//     // // {
+//     // //     let x = 5;
+//     // //     r = &x;
+//     // // }
+
+//     // println!("r: {r}");
+// }
+
+// fn longest<'a>(x: &str, y: &str) -> &'a str {
+//     let result = String::from("really long string");
+//     result.as_str()
+//     // x
+//     // if x.len() > y.len() { x } else { y }
+// }
