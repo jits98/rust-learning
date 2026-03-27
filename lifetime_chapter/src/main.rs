4eca1307794d1c70f6bf8f3f7e57d34bb8dea8bf
jits98 <jits8809@gmail.com>
2026-03-27 11:22:38 +0530

    example 10-22: Using the longest function with references to String values that have different concrete lifetimes

--- lifetime_chapter/src/main.rs
+++ lifetime_chapter/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
+    let string1 = String::from("long string is long");
+    let string2 = String::from("xyz");
 
-    let result = longest(string1.as_str(), string2);
+    let result = longest(string1.as_str(), string2.as_str());
     println!("The longest string is {result}");
 
     // let x = 5;
