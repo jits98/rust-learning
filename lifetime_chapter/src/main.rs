df338d5386fce262dc8efdaf617f3df5d6c66329
jits98 <jits8809@gmail.com>
2026-03-27 10:45:57 +0530

    example 10-19: A main function that calls the longest function to find the longer of two string slices

--- lifetime_chapter/src/main.rs
+++ lifetime_chapter/src/main.rs
@@ -1,12 +1,22 @@
 fn main() {
-    let x = 5;
-    
-    let r = &x;
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
 
-    // {
-    //     let x = 5;
-    //     r = &x;
-    // }
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {result}");
 
-    println!("r: {r}");
+    // let x = 5;
+
+    // let r = &x;
+
+    // // {
+    // //     let x = 5;
+    // //     r = &x;
+    // // }
+
+    // println!("r: {r}");
+}
+
+fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() { x } else { y }
 }
