73b7ffc34aa89cda3f6e0c793387eb5c1f0c1876
jits98 <jits8809@gmail.com>
2026-03-28 15:48:36 +0530

    function example

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,89 +1,34 @@
-#[derive(Debug)]
-struct Book {
-    title: String,
-    author: String,
-    pages: u32,
-}
-
-impl Book {
-    fn is_long(&self) -> bool {
-        self.pages > 300
-    }
-
-    fn same_author(&self, other: &Book) -> bool {
-        self.author == other.author
-    }
-
-    fn describe(&self) -> String {
-        format!("{} by {}, and total pages in the book are {}", self.title, self.author, self.pages)
-        
-        // self.title.clone() + " by " + &self.author + ", " + &self.pages.to_string();
-
-    }
-
-    fn new(title: &str, author: &str, pages: u32) -> Book {
-        Book { 
-            title: title.to_string(), 
-            author: author.to_string(), 
-            pages 
-        }
-    }
-
-    fn is_longer_than(&self, other: &Book) -> bool {
-        self.pages > other.pages      
-    }
-}
-
+// use std::io;
 
 fn main() {
-    let book1 = Book::new("Exercised", "Daniel", 350);
+    let result = add(5,3);
+    println!("{}", result);
 
-    let book2 = Book::new("My life in full", "Indira Nooyi", 280);
+    // let tup = (500, "hello", true);
+    // let (x,y,z) = tup;
+    // let first = tup.0;
 
-    let book3 = Book::new("Edible Economics", "Ho joon chong", 320);
+    // let arr = [1,2,3,4,5];
+    // let first = arr[0];
+    // let mut guess = String::new();
 
-    println!("{}", book1.describe());
-    println!("{}", book2.describe());
-    println!("{}", book3.describe());
+    // io::stdin()
+    //     .read_line(&mut guess)
+    //     .expect("Failed to read");
 
-    println!("The first is more than 300 pages: {}", book1.is_long());
-    println!("The second book is more than 300 pages: {}", book2.is_long());
-    println!("The third book is more than 300 pages: {}", book3.is_long());
+    // let guess: u32 = guess.trim().parse().expect("Not a number!");
 
-    println!("The first book is more lengthy than second book: {}", book1.is_longer_than(&book2));
+    // match guess.cmp(&secret_number) {
+    //     Ordering::Less => println!("Too small"),
+    //     Ordering::Greater => println!("Too big!"),
+    //     Ordering::Equal => println!("You win!"),
+    // }
 
-    println!("The first and second book has same author: {}", book1.same_author(&book2));
-
-    let max_dog_age = Dog::max_age();
-    println!("{}", max_dog_age);
-    let my_dog = Dog::new("Rex", 5);
-    println!("{:?}", my_dog);
-
-    println!("{}", my_dog.bark());
-    
 }
 
-#[derive(Debug)]
-struct Dog {
-    name: String,
-    age: u32,
-}
 
-impl Dog {
-    fn bark(&self) -> String {
-        format!("{} says woof!", self.name)
-    }
-
-    fn new(name: &str, age: u32) -> Dog {
-        Dog {
-            name: name.to_string(),
-            age
-        }
-    }
-
-    fn max_age() -> u32 {
-        20
-    }
+fn add(x: i32, y: i32) -> i32 {
+    x + y
 }
 
 
@@ -115,166 +60,336 @@ impl Dog {
 
 
 
-// fn non_repeating(s: &str) -> char {
-//     let new;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #[derive(Debug)]
+// struct Book {
+//     title: String,
+//     author: String,
+//     pages: u32,
 // }
 
+// impl Book {
+//     fn is_long(&self) -> bool {
+//         self.pages > 300
+//     }
 
+//     fn same_author(&self, other: &Book) -> bool {
+//         self.author == other.author
+//     }
 
-// fn reverse_vector(v: Vec<i32>) -> Vec<i32> {
-//     let reversed: Vec<i32> = v.into_iter().rev().collect();
+//     fn describe(&self) -> String {
+//         format!("{} by {}, and total pages in the book are {}", self.title, self.author, self.pages)
+        
+//         // self.title.clone() + " by " + &self.author + ", " + &self.pages.to_string();
 
-//     reversed
-// }
+//     }
 
-// fn main() {
-//     let vec = vec![1,2,3,4];
-
-//     println!("The reverse vector is: {:?}", reverse_vector(vec));
-// }
-
-
-// fn count_chars(s: &str) -> usize {
-//     s.len()
-// }
-
-// fn main() {
-//     let word = String::from("rust");
-
-//     println!("The number of characters in word: {}", count_chars(&word));
-// }
-
-
-
-// fn even_number(v: &Vec<i32>) -> Vec<i32> {
-//     let mut even_vec = Vec::new();
-//     for num in v {
-//         if num % 2 == 0 {
-//             even_vec.push(*num);
+//     fn new(title: &str, author: &str, pages: u32) -> Book {
+//         Book { 
+//             title: title.to_string(), 
+//             author: author.to_string(), 
+//             pages 
 //         }
 //     }
-//     even_vec
-// }
 
-// fn main() {
-//     let list = vec![1,2,3,4,5,6];
-
-//     println!("The even numbers in list: {:?}", even_number(&list));
+//     fn is_longer_than(&self, other: &Book) -> bool {
+//         self.pages > other.pages      
+//     }
 // }
 
 
 // fn main() {
-//     let rect1 = Rectangle {
-//         width: 20.00,
-//         height: 30.00,
-//     };
-//     let rect2 = Rectangle {width: 30.00, height:40.00};
+//     let book1 = Book::new("Exercised", "Daniel", 350);
 
-//     let square = Rectangle::square(10.00);
+//     let book2 = Book::new("My life in full", "Indira Nooyi", 280);
 
-//     println!("{:?}", rect1);
+//     let book3 = Book::new("Edible Economics", "Ho joon chong", 320);
 
-//     println!("Area of Rectangle: {:?}", rect1.area());
+//     println!("{}", book1.describe());
+//     println!("{}", book2.describe());
+//     println!("{}", book3.describe());
 
-//     println!("The answer of holding is: {}", rect1.can_hold(&rect2));
+//     println!("The first is more than 300 pages: {}", book1.is_long());
+//     println!("The second book is more than 300 pages: {}", book2.is_long());
+//     println!("The third book is more than 300 pages: {}", book3.is_long());
 
-//     println!("The square is: {:?} ", square);
+//     println!("The first book is more lengthy than second book: {}", book1.is_longer_than(&book2));
+
+//     println!("The first and second book has same author: {}", book1.same_author(&book2));
+
+//     let max_dog_age = Dog::max_age();
+//     println!("{}", max_dog_age);
+//     let my_dog = Dog::new("Rex", 5);
+//     println!("{:?}", my_dog);
+
+//     println!("{}", my_dog.bark());
+    
 // }
 
 // #[derive(Debug)]
-// struct Rectangle {
-//     width: f64,
-//     height: f64,
+// struct Dog {
+//     name: String,
+//     age: u32,
 // }
 
-// impl Rectangle {
-//     fn area(&self) -> f64 {
-//         self.width * self.height
+// impl Dog {
+//     fn bark(&self) -> String {
+//         format!("{} says woof!", self.name)
 //     }
 
-//     fn can_hold(&self, other: &Rectangle) -> bool {
-//         self.width > other.width && self.height > other.height
-//     }
-
-//     fn square(size: f64) -> Rectangle {
-//         Rectangle { width: size, height : size}
-//     } 
-// }
-
-
-
-// use std::io;
-
-// fn main() {
-//     fizzbuzz();
-    
-
-// }
-
-// fn fizzbuzz() {
-
-//     println!("Enter a number between 1 and 100 for print fizzbuzz");
-
-//     let mut input = String::new();
-
-//     io::stdin()
-//         .read_line(&mut input)
-//         .expect("Failed to read the input");
-
-//     let num = input.trim().parse().expect("Please enter a valid integer");
-
-//     if num < 1 || num > 100 {
-//         println!("Please enter a number between 1 and 100")
-//     };
-
-
-//     for number in 1..=num {
-//         if number % 3 == 0 && number % 5 == 0 {
-//             println!("FizzBuzz");
-//         } else if number % 3 == 0 {
-//             println!("Fizz");
-//         } else if number % 5 == 0 {
-//             println!("Buzz");
-//         } else {
-//             println!("{}", number)
+//     fn new(name: &str, age: u32) -> Dog {
+//         Dog {
+//             name: name.to_string(),
+//             age
 //         }
+//     }
+
+//     fn max_age() -> u32 {
+//         20
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // fn non_repeating(s: &str) -> char {
+// //     let new;
+// // }
+
+
+
+// // fn reverse_vector(v: Vec<i32>) -> Vec<i32> {
+// //     let reversed: Vec<i32> = v.into_iter().rev().collect();
+
+// //     reversed
+// // }
+
+// // fn main() {
+// //     let vec = vec![1,2,3,4];
+
+// //     println!("The reverse vector is: {:?}", reverse_vector(vec));
+// // }
+
+
+// // fn count_chars(s: &str) -> usize {
+// //     s.len()
+// // }
+
+// // fn main() {
+// //     let word = String::from("rust");
+
+// //     println!("The number of characters in word: {}", count_chars(&word));
+// // }
+
+
+
+// // fn even_number(v: &Vec<i32>) -> Vec<i32> {
+// //     let mut even_vec = Vec::new();
+// //     for num in v {
+// //         if num % 2 == 0 {
+// //             even_vec.push(*num);
+// //         }
+// //     }
+// //     even_vec
+// // }
+
+// // fn main() {
+// //     let list = vec![1,2,3,4,5,6];
+
+// //     println!("The even numbers in list: {:?}", even_number(&list));
+// // }
+
+
+// // fn main() {
+// //     let rect1 = Rectangle {
+// //         width: 20.00,
+// //         height: 30.00,
+// //     };
+// //     let rect2 = Rectangle {width: 30.00, height:40.00};
+
+// //     let square = Rectangle::square(10.00);
+
+// //     println!("{:?}", rect1);
+
+// //     println!("Area of Rectangle: {:?}", rect1.area());
+
+// //     println!("The answer of holding is: {}", rect1.can_hold(&rect2));
+
+// //     println!("The square is: {:?} ", square);
+// // }
+
+// // #[derive(Debug)]
+// // struct Rectangle {
+// //     width: f64,
+// //     height: f64,
+// // }
+
+// // impl Rectangle {
+// //     fn area(&self) -> f64 {
+// //         self.width * self.height
+// //     }
+
+// //     fn can_hold(&self, other: &Rectangle) -> bool {
+// //         self.width > other.width && self.height > other.height
+// //     }
+
+// //     fn square(size: f64) -> Rectangle {
+// //         Rectangle { width: size, height : size}
+// //     } 
+// // }
+
+
+
+// // use std::io;
+
+// // fn main() {
+// //     fizzbuzz();
     
-// }
-// }
 
-// fn add(a: i32, b: i32) -> i32 {
-//     a + b
-// }
+// // }
 
-// fn greet(name: &str) -> String {
-//     let greeting = String::from("Hello, ") + name;
-//     greeting
-// }
+// // fn fizzbuzz() {
 
-// fn is_even(num: i32) -> bool {
-//     num % 2 == 0
-// }
+// //     println!("Enter a number between 1 and 100 for print fizzbuzz");
+
+// //     let mut input = String::new();
+
+// //     io::stdin()
+// //         .read_line(&mut input)
+// //         .expect("Failed to read the input");
+
+// //     let num = input.trim().parse().expect("Please enter a valid integer");
+
+// //     if num < 1 || num > 100 {
+// //         println!("Please enter a number between 1 and 100")
+// //     };
+
+
+// //     for number in 1..=num {
+// //         if number % 3 == 0 && number % 5 == 0 {
+// //             println!("FizzBuzz");
+// //         } else if number % 3 == 0 {
+// //             println!("Fizz");
+// //         } else if number % 5 == 0 {
+// //             println!("Buzz");
+// //         } else {
+// //             println!("{}", number)
+// //         }
+    
+// // }
+// // }
+
+// // fn add(a: i32, b: i32) -> i32 {
+// //     a + b
+// // }
+
+// // fn greet(name: &str) -> String {
+// //     let greeting = String::from("Hello, ") + name;
+// //     greeting
+// // }
+
+// // fn is_even(num: i32) -> bool {
+// //     num % 2 == 0
+// // }
 
 
 
-    // let mut your_age: i32 = 26;
-    // println!("My age is: {}", your_age);
+//     // let mut your_age: i32 = 26;
+//     // println!("My age is: {}", your_age);
 
-    // let height: f64 = 1.72;
-    // println!("My height in meters: {}", height);
+//     // let height: f64 = 1.72;
+//     // println!("My height in meters: {}", height);
 
-    // let name = String::from("Jitendra Singh");
-    // println!("My name is {}", name);
+//     // let name = String::from("Jitendra Singh");
+//     // println!("My name is {}", name);
 
-    // let whether_you_like_coding = true;
-    // println!("I like coding which is {}", whether_you_like_coding);
+//     // let whether_you_like_coding = true;
+//     // println!("I like coding which is {}", whether_you_like_coding);
 
-    // your_age = 27;
-    // println!("My age is {}", your_age);
+//     // your_age = 27;
+//     // println!("My age is {}", your_age);
 
-    // println!("The sum of 13 and 12 is {}", add(13, 12));
+//     // println!("The sum of 13 and 12 is {}", add(13, 12));
 
-    // let name = String::from("Jitendra Singh");
-    // println!("{}", greet(&name));
+//     // let name = String::from("Jitendra Singh");
+//     // println!("{}", greet(&name));
 
-    // println!("{}", is_even(5));
+//     // println!("{}", is_even(5));
