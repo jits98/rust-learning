9f6460f70aacd019cd326314a84f7f0205d0eed4
jits98 <jits8809@gmail.com>
2026-03-31 17:40:49 +0530

    example 11-8: Testing that a condition will cause a panic!

--- test_chapter/src/lib.rs
+++ test_chapter/src/lib.rs
@@ -1,20 +1,48 @@
-pub fn add_two(a: u64) -> u64 {
-  a + 2
+pub struct Guess {
+  value: i32,
 }
 
+impl Guess {
+  pub fn new(value: i32) -> Guess {
+    if value < 1 || value > 100 {
+      panic!("Guess value must be between 1 and 100, got {value}.");
+    }
+    Guess{ value }
+  }
+}
 
 #[cfg(test)]
 mod tests {
   use super::*;
-  
+
   #[test]
-  fn it_adds_two() {
-    let result = add_two(2);
-    assert_eq!(result, 4);
+  #[should_panic]
+  fn greater_than_100() {
+    Guess::new(200);
   }
-  
 }
 
+
+// #[test]
+// fn greeting_contains_name() {
+//   let result = greeting("Carol");
+//   assert!(
+//     result.contains("Carol"),
+//     "Greeting did not contain name, value was '{result}'"
+//   );
+// }
+// pub fn greeting(name: &str) -> String  {
+//   String::from("Hello!")
+//   // format!("Hello {name}!")
+// }
+// pub fn add_two(a: u64) -> u64 {
+//   a + 3
+// }
+// #[test]
+// fn it_adds_two() {
+//   let result = add_two(2);
+//   assert_eq!(result, 4);
+// }
 // #[test]
 // fn exploration() {
 //   let result = add(2, 2);
