93d90c014fe1e1ea935bd1c86c3a0b9006107f5a
jits98 <jits8809@gmail.com>
2026-03-31 17:45:43 +0530

    example 11-9: Testing for a panic! with a panic message containing a specified substring

--- test_chapter/src/lib.rs
+++ test_chapter/src/lib.rs
@@ -4,8 +4,12 @@ pub struct Guess {
 
 impl Guess {
   pub fn new(value: i32) -> Guess {
-    if value < 1 || value > 100 {
+    if value < 1 {
       panic!("Guess value must be between 1 and 100, got {value}.");
+    } else if value > 100 {
+      panic!(
+        "Guess value must be less than or equal to 100, got {value}."
+      );
     }
     Guess{ value }
   }
@@ -16,7 +20,7 @@ mod tests {
   use super::*;
 
   #[test]
-  #[should_panic]
+  #[should_panic(expected = "less than or equal to 100")]
   fn greater_than_100() {
     Guess::new(200);
   }
