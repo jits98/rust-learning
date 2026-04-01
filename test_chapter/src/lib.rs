7fda8d0ad22edd12e4be292376762b89fbdb8564
jits98 <jits8809@gmail.com>
2026-04-01 08:29:56 +0530

    example 11-11: Three tests with three different names

--- test_chapter/src/lib.rs
+++ test_chapter/src/lib.rs
@@ -1,6 +1,5 @@
-fn prints_and_returns_10(a: i32) -> i32 {
-  println!("I got the value {a}");
-  10
+pub fn add_two(a: u64) -> u64 {
+  a + 2
 }
 
 #[cfg(test)]
@@ -8,17 +7,45 @@ mod tests {
   use super::*;
 
   #[test]
-  fn this_test_will_pass() {
-    let value = prints_and_returns_10(4);
-    assert_eq!(value, 10);
+  fn add_two_and_two() {
+    let result = add_two(2);
+    assert_eq!(result, 4);
   }
 
   #[test]
-  fn this_test_will_fail() {
-    let value = prints_and_returns_10(8);
-    assert_eq!(value, 5);
+  fn add_three_and_two() {
+    let result = add_two(3);
+    assert_eq!(result, 5);
+  }
+
+  #[test]
+  fn one_hundred() {
+    let result = add_two(100);
+    assert_eq!(result, 102);
   }
 }
+
+// fn prints_and_returns_10(a: i32) -> i32 {
+//   println!("I got the value {a}");
+//   10
+// }
+
+// #[cfg(test)]
+// mod tests {
+//   use super::*;
+
+//   #[test]
+//   fn this_test_will_pass() {
+//     let value = prints_and_returns_10(4);
+//     assert_eq!(value, 10);
+//   }
+
+//   #[test]
+//   fn this_test_will_fail() {
+//     let value = prints_and_returns_10(8);
+//     assert_eq!(value, 5);
+//   }
+// }
 
 
 
