6367ccd5a97d3910aa3d8970e535273a8a48a094
jits98 <jits8809@gmail.com>
2026-04-02 10:57:03 +0530

    start 11.3: Test Organization chapter

--- test_chapter/src/lib.rs
+++ test_chapter/src/lib.rs
@@ -1,5 +1,5 @@
-pub fn add(a: i32, b: i32) -> i32 {
-  a + b
+pub fn add(left: u64, right: u64) -> u64 {
+  left + right
 }
 
 #[cfg(test)]
@@ -8,17 +8,33 @@ mod tests {
 
   #[test]
   fn it_works() {
-    let result = add(2, 2);
+    let result = add(2,2);
     assert_eq!(result, 4);
   }
-
-  #[test]
-  #[ignore]
-  fn expensive_test() {}
 }
 
 
 
+
+
+// pub fn add(a: i32, b: i32) -> i32 {
+//   a + b
+// }
+
+// #[cfg(test)]
+// mod tests {
+//   use super::*;
+
+//   #[test]
+//   fn it_works() {
+//     let result = add(2, 2);
+//     assert_eq!(result, 4);
+//   }
+
+//   #[test]
+//   #[ignore]
+//   fn expensive_test() {}
+// }
 
 // pub fn add_two(a: u64) -> u64 {
 //   a + 2
