eb066f180497ab13610474f4a218b26cecb0db4e
jits98 <jits8809@gmail.com>
2026-04-02 11:01:17 +0530

    example 11-12: Testing a private function

--- test_chapter/src/lib.rs
+++ test_chapter/src/lib.rs
@@ -1,4 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
+pub fn add_two(a: u64) -> u64 {
+  internal_adder(a, 2)
+}
+
+fn internal_adder(left: u64, right: u64) -> u64 {
   left + right
 }
 
@@ -7,8 +11,8 @@ mod tests {
   use super::*;
 
   #[test]
-  fn it_works() {
-    let result = add(2,2);
+  fn internal() {
+    let result = internal_adder(2, 2);
     assert_eq!(result, 4);
   }
 }
@@ -16,6 +20,23 @@ mod tests {
 
 
 
+
+
+
+// pub fn add(left: u64, right: u64) -> u64 {
+//   left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//   use super::*;
+
+//   #[test]
+//   fn it_works() {
+//     let result = add(2,2);
+//     assert_eq!(result, 4);
+//   }
+// }
 
 // pub fn add(a: i32, b: i32) -> i32 {
 //   a + b
