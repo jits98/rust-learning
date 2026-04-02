8fa9e452e8f44a18fc5dad3d45071e8d75c32b20
jits98 <jits8809@gmail.com>
2026-04-02 11:07:40 +0530

    example 11-13: An integration test of a function in the adder crate

---
+++ test_chapter/target/debug/deps/integration_test-c3b38cafbcf1798d.d
@@ -0,0 +1,5 @@
+/Users/home/Desktop/projects/test_chapter/target/debug/deps/integration_test-c3b38cafbcf1798d.d: tests/integration_test.rs
+
+/Users/home/Desktop/projects/test_chapter/target/debug/deps/libintegration_test-c3b38cafbcf1798d.rmeta: tests/integration_test.rs
+
+tests/integration_test.rs:
